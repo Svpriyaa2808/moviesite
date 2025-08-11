baa24c8d174d1e477b8a4ea836f929eda80055d3
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-08-11 11:30:05 +0200

    Adding Header component

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <>
+        <div>
+        <h1>Welcome to Movie site</h1>   
+        </div>
+        </>
+    )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-  <div>Hello</div>
+    <>
+    <Header />
+    </>
+  
+
   );
 }
