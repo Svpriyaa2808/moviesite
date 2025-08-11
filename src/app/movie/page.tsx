64603e88c92f6dda2e1524eff3ca4e0a46e225c7
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-08-11 21:21:30 +0200

    fixing flex wrap

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -5,7 +5,7 @@ import { moviedata } from "../data/movieData"
 const Movie = () => {
     
     return (
-        <div className=" bg-gray-800">
+        <div className="flex flex-col lg:flex-row flex-wrap bg-gray-800">
            {moviedata.map((item,index) => <MovieDescription key={index} {...item} />)}      
         </div>
     )
