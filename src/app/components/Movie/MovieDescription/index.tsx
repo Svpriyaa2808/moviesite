112ec55a78a01a1b249c91b4601b24bcf551b710
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-08-12 21:24:16 +0200

    adding Favlogic

--- src/app/components/Movie/MovieDescription/index.tsx
+++ src/app/components/Movie/MovieDescription/index.tsx
@@ -1,35 +1,12 @@
-'use client'
-import { FavArrayList } from "@/app/data/movieData"
 import { movieDataType } from "@/app/utils/types"
-import { useEffect, useState } from "react"
 
 interface MovieDescriptionProps extends movieDataType {
     addFav:(movieName:string)=>void
     handleIcon:string
 }
 
-
 const MovieDescription = ({icon, movieName, releaseYear, genre, favourite,addFav,handleIcon}: MovieDescriptionProps) => {
-    
-   
-    let favouriteMovieList:movieDataType[] =[];
-
-    // const addFav = (movieName:string) => {
-    //     if(fav === "unfavourite_icon.png"){
-    //         setFav("favourite_icon.png")
-    //         // setFavArray(FavArrayList(movieName))
-    //          const newFavs = [...favArray, ...FavArrayList(movieName)]; // Make sure FavArrayList returns movieDataType
-    //          setFavArray(newFavs);
-    //          console.log(newFavs)
-            
-    //     }else {
-    //         setFav("unfavourite_icon.png")
-    //     }
-    // }
-
-
-    return(
-        
+    return(  
         <div className="flex-[45%] m-4 flex flex-col align-center sm:flex-row  rounded-4xl items-center bg-blue-50  p-4">
             <div className="flex-[20%]">
                 <img className="w-[50%] sm:w-[100%] h-[auto] m-auto sm:ml-8" src={icon} alt={movieName} width={500} height={100}/>
@@ -41,14 +18,7 @@ const MovieDescription = ({icon, movieName, releaseYear, genre, favourite,addFav
                 </div>
                <div className="flex-[20%]">
                 <img className="w-[25%] sm:w-[35%] h-[auto] cursor-pointer m-auto " onClick={() => addFav(movieName)}  src={handleIcon} alt="heart_icon"></img>
-                 </div>
-            {/* {favArray.map((item,index)=> < div key={index}>
-            <h4 className="font-bold text-2xl uppercase">{item.movieName}</h4>
-                <p className="text-[22px] mt-4">Year:{item.releaseYear} </p>
-                <p className="text-[22px] mt-4">Genre:{item.genre}</p>
-            </div>)} */}
-
-                
+                 </div> 
         </div>
     )
 }
--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -10,7 +10,6 @@ const Movie = () => {
     const [filterSelected, setFilterSelected] = useState<null | string[]>(null)
     const [genreMovieList, setGenreMovieList] = useState<null | movieDataType[]>(null)
     const [favArray, setFavArray] = useState<[] | movieDataType[]>([])
-    const [fav, setFav] = useState<string>("unfavourite_icon.png")
 
     const handleFilter = (): void => {
         filterSelected === null ? setFilterSelected(filteredGenres) : setFilterSelected(null)
@@ -26,52 +25,41 @@ const Movie = () => {
         setGenreMovieList(null)
     }
 
-    const favIcon = () => {
-        if (fav === "unfavourite_icon.png") {
-            setFav("favourite_icon.png")
-        } else {
-            setFav("unfavourite_icon.png")
-        }
-    }
-
     const assignFav = (movieName: string) => {
-        // Check if the movie is already in favourites
-
         const exists = favArray.some((m) => m.movieName === movieName);
 
         if (!exists) {
-            // Add to favourites
-            // setFav("favourite_icon.png");
+
             const newFavs = [...favArray, ...FavArrayList(movieName)]; // Make sure FavArrayList returns movieDataType
             setFavArray(newFavs);
             console.log(newFavs)
         } else {
-            // Remove from favourites
-            // setFav("unfavourite_icon.png");
             const newFavs = favArray.filter((m) => m.movieName !== movieName);
             setFavArray(newFavs);
             console.log(newFavs)
         }
     }
+    const getIconForMovie = (movieName: string) => {
+        const isFav = favArray.some(m => m.movieName === movieName);
+        return isFav ? "favourite_icon.png" : "unfavourite_icon.png";
+    };
 
 
-        return (
-            <>
-                <button onClick={handleFilter}>Filter</button>
-                <button onClick={handleClear}>Back</button>
-                {filterSelected && filteredGenres.map((item, index) => <div key={index} onClick={() => chosenGenre(item)}>{item}</div>)}
+    return (
+        <>
+            <button onClick={handleFilter}>Filter</button>
+            <button onClick={handleClear}>Back</button>
+            {filterSelected && filteredGenres.map((item, index) => <div key={index} onClick={() => chosenGenre(item)}>{item}</div>)}
 
-                <div className="flex flex-col lg:flex-row flex-wrap bg-gray-800">
-                    {genreMovieList && genreMovieList.map((item: movieDataType, index: number) => <MovieDescription key={index} {...item} handleIcon={setFav}  addFav={assignFav} />)}
+            <div className="flex flex-col lg:flex-row flex-wrap bg-gray-800">
+                {genreMovieList && genreMovieList.map((item: movieDataType, index: number) => <MovieDescription key={index} {...item} handleIcon={getIconForMovie(item.movieName)} addFav={assignFav} />)}
 
-                    {!genreMovieList &&
-                        moviedata.map((item: movieDataType, index: number) => <MovieDescription key={index} {...item} handleIcon={setFav} addFav={assignFav} />)
-                    }
-                </div>
-
-
-            </>
-        )
-    }
+                {!genreMovieList &&
+                    moviedata.map((item: movieDataType, index: number) => <MovieDescription key={index} {...item} handleIcon={getIconForMovie(item.movieName)} addFav={assignFav} />)
+                }
+            </div>
+        </>
+    )
+}
 
 export default Movie 
