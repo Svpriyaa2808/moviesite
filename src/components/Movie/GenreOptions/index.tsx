2fdedaa1fac305e391b72b9049f24a914f885d35
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-08-14 12:15:40 +0200

    adding bakbutton as separate component and testing

--- src/components/Movie/GenreOptions/index.tsx
+++ src/components/Movie/GenreOptions/index.tsx
@@ -1,11 +1,49 @@
+'use client'
+import { useState } from "react"
 
+type MovieFilterProps = {
+    filteredGenres: string[];
+    onGenreSelect: (genre: string) => void;
+};
 
-const GenreOptions = () => {
-    return(
-        <div>
-          
-        </div>
-    )
-}
+const GenreOptions = ({ filteredGenres, onGenreSelect}: MovieFilterProps) => {
+    const [filterSelected, setFilterSelected] = useState<null | string[]>(null);
 
-export default GenreOptions
+    const handleFilterToggle = () => {
+        setFilterSelected(filterSelected === null ? filteredGenres : null);
+    };
+
+    const handleClick = (item:string) => {
+        onGenreSelect(item);
+                                    setFilterSelected(null);
+    }
+
+    return (
+        
+            <div className="relative inline-block">
+                <button
+                    className="bg-gray-800 text-white cursor-pointer p-6 min-w-[150px] text-[20px] font-bold rounded-2xl"
+                    onClick={handleFilterToggle}
+                >
+                    Filter By Genre
+                </button>
+
+                {filterSelected && (
+                    <div className="absolute left-0 mt-2 w-full bg-white rounded-lg shadow-lg overflow-hidden">
+                        {filteredGenres.map((item, index) => (
+                            <div
+                                className="bg-blue-50 hover:bg-blue-100 cursor-pointer text-[20px] font-bold"
+                                key={index}
+                                onClick={() =>handleClick(item)}
+                            >
+                                <p className="border-b-2 text-center">{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+    
+    );
+};
+
+export default GenreOptions;
