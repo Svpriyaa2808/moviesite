3d26459bf76aef5387130aaf8f0c6764e127c7b6
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-08-14 02:55:52 +0200

    adding changes to test in Movie Page

--- src/__test__/Movie.test.tsx
+++ src/__test__/Movie.test.tsx
@@ -1,12 +1,15 @@
-import {render,screen} from '@testing-library/react'
+import {render,screen, within} from '@testing-library/react'
 import Movie from '@/app/movie/page'
+import { moviedata } from '@/data/movieData'
 
 describe("Test the element to be present on page load ",()=> {
-    test("test the number of movie to be present on page load",()=>{
+    test("Test the number of movies to be present on page load",()=>{
         render(<Movie />)
 
         const allMovies = screen.getAllByTestId("movie")
-        expect(allMovies.length).toBe(18)
+        expect(allMovies.length).toBe(moviedata.length)
+
+        
     }),
     
     test("That filter button is present",()=>{
@@ -21,5 +24,21 @@ describe("Test the element to be present on page load ",()=> {
 
         const backButton = screen.getByRole("button",{name:"Back"})
         expect(backButton).toBeInTheDocument()
+    })
+})
+
+describe("To check if the data matches exactly with the the dataset", () => {
+    test("If the data is rendered is in coherence with the dataset",() => {
+        render(<Movie />)
+
+        let allMovieData= screen.getAllByTestId("movie")
+        expect(allMovieData.length).toBe(moviedata.length)
+
+        allMovieData.forEach((movie,index) => {
+            let movieDetail =moviedata[index];
+
+            expect(within(movie).getByText(movieDetail.movieName)).toBeInTheDocument()
+        
+        })
     })
 })
