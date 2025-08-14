fd6cd9317ec1c7f69e48203a15da131850c76ddd
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-08-14 02:01:36 +0200

    addng HomePage title check

--- src/__test__/Home_Page.test.tsx
+++ src/__test__/Home_Page.test.tsx
@@ -9,4 +9,11 @@ describe("To check if the Home Page has the Heading as expected",() => {
         expect(home_title).toBeInTheDocument()
         expect(home_title).toHaveTextContent("Welcome to Popcorn Picks")
     })
+
+      test("To check if the Home Page paragraph is getting rendered as expected", () => {
+        render(<Home />)
+        let home_text = screen.getByRole("paragraph")
+        expect(home_text).toBeInTheDocument()
+        
+    })
 })
